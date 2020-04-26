4c5c432347a7ebe7edfe39424309fcd58241c848
abdelhamid1A <57224844+abdelhamid1A@users.noreply.github.com>
2020-04-26 23:03:24 +0000

    Update client.sql

--- client.sql
+++ client.sql
@@ -2,3 +2,8 @@ SELECT * FROM newschema.client;
 INSERT INTO client
 VALUES (2,'zz','xx','dd');
 SELECT * FROM newschema.client;
+/*update */
+UPDATE client
+SET nom = 'azizi abdelhamid', email= 'aa@gmail.com'
+WHERE id_client = 1;
+SELECT * FROM newschema.client;
